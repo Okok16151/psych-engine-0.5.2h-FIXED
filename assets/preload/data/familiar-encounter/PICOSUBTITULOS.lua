a55da66ccbf52e4d1ccae734bb90dbd4969fc322
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-06-30 20:08:55 +0800

    Add files via upload

---
+++ assets/preload/data/familiar-encounter/PICOSUBTITULOS.lua
@@ -0,0 +1,252 @@
+function onStepHit()--Bueno Este Metodo Es Muy Facil De Usar, Lo Cree En Un Dia Si me Dan creditos Por El Esfuerso Se Los Agradeceria Soy Perez Sen Y Eso Es TODO :)
+	if curStep == 164 then
+		makeLuaText('Text', "Well, Fuck you too then asshole", 600, 150, 500); 
+		setTextSize('Text', 27);
+		setObjectCamera('Text', 'other');
+		addLuaText('Text', true);
+	end
+	if curStep == 181 then
+		makeLuaText('Text2', "Heh, heh, heh", 600, 150, 500); 
+		setTextSize('Text2', 27);
+		setObjectCamera('Text2', 'other');
+		addLuaText('Text2', true);
+
+		setProperty('Text.visible', false);
+	end
+		if curStep == 192 then
+			setProperty('Text2.visible', false);
+
+	  end
+	  if curStep == 609 then
+		makeLuaText('Text3', "Gonna take you to bed", 600, 670, 530); 
+		setTextSize('Text3', 29);
+		setTextBorder("Text3", 1, '85E100');
+		setObjectCamera('Text3', 'other');
+		addLuaText('Text3', true);
+
+	end
+	if curStep == 626 then
+		makeLuaText('Text4', "gonna make some blood shed", 600, 670, 530); 
+		setTextSize('Text4', 29);
+		setTextBorder("Text4", 1, '85E100');
+		setObjectCamera('Text4', 'other');
+		addLuaText('Text4', true);
+
+		setProperty('Text3.visible', false);
+	  end
+	  if curStep == 641 then
+		makeLuaText('Text5', "You're about to get blammed", 600, 670, 530); 
+		setTextSize('Text5', 31);
+		setTextBorder("Text5", 1, '85E100');
+		setObjectCamera('Text5', 'other');
+		addLuaText('Text5', true);
+
+		setProperty('Text4.visible', false);
+	  end
+	  if curStep == 658 then
+		makeLuaText('Text7', "and then you'll be Damned", 600, 670, 530); 
+		setTextSize('Text7', 29);
+		setTextBorder("Text7", 1, '85E100');
+		setObjectCamera('Text7', 'other');
+		addLuaText('Text7', true);
+		setProperty('Text5.visible', false);
+	  end
+		if curStep == 672 then
+			setProperty('Text7.visible', false);
+
+	  end
+	  if curStep == 704 then
+		makeLuaText('Text8', "Hi, Pico!", 600, 670, 600); 
+		setTextSize('Text8', 29);
+		setTextBorder("Text8", 1, '00E8FF');
+		setObjectCamera('Text8', 'other');
+		addLuaText('Text8', true);
+	  end
+	  if curStep == 714 then
+		makeLuaText('Text9', "BOYFRIEND?", 600, 670, 530); 
+		setTextSize('Text9', 29);
+		setTextBorder("Text9", 1, '85E100');
+		setObjectCamera('Text9', 'other');
+		addLuaText('Text9', true);
+		
+		setProperty('Text8.visible', false);
+	  end
+	  if curStep == 721 then
+		makeLuaText('Text10', "What are you doing here?", 600, 670, 530); 
+		setTextSize('Text10', 29);
+		setTextBorder("Text10", 1, '85E100');
+		setObjectCamera('Text10', 'other');
+		addLuaText('Text10', true);
+
+		setProperty('Text9.visible', false);
+	  end
+	  if curStep == 732 then
+		makeLuaText('Text11', "amogus is very sus", 600, 670, 600); 
+		setTextSize('Text11', 29);
+		setTextBorder("Text11", 1, '00E8FF');
+		setObjectCamera('Text11', 'other');
+		addLuaText('Text11', true);
+
+		setProperty('Text10.visible', false);
+	  end
+	  if curStep == 745 then
+		makeLuaText('Text12', "y'know what?", 600, 670, 530); 
+		setTextSize('Text12', 29);
+		setTextBorder("Text12", 1, '85E100');
+		setObjectCamera('Text12', 'other');
+		addLuaText('Text12', true);
+
+		setProperty('Text11.visible', false);
+	  end
+	  if curStep == 751 then
+		makeLuaText('Text13', "whatever", 600, 670, 530); 
+		setTextSize('Text13', 29);
+		setTextBorder("Text13", 1, '85E100');
+		setObjectCamera('Text13', 'other');
+		addLuaText('Text13', true);
+
+		setProperty('Text12.visible', false);
+	  end
+	  if curStep == 758 then
+		makeLuaText('Text14', "just come sing with me", 600, 670, 530);
+		setTextSize('Text14', 29);
+		setTextBorder("Text14", 1, '85E100');
+		setObjectCamera('Text14', 'other');
+		addLuaText('Text14', true);
+
+		setProperty('Text13.visible', false);
+	  end
+	  if curStep == 768 then
+		makeLuaText('Text15', "What the hell?", 600, 150, 500); 
+		setTextSize('Text15', 29);
+		setObjectCamera('Text15', 'other');
+		addLuaText('Text15', true);
+
+		setProperty('Text14.visible', false);
+	  end
+	  if curStep == 777 then
+		makeLuaText('Text16', "2 Vs 1?", 600, 150, 500);
+		setTextSize('Text16', 29);
+		setObjectCamera('Text16', 'other');
+		addLuaText('Text16', true);
+
+		setProperty('Text15.visible', false);
+	  end
+	  if curStep == 785 then
+		makeLuaText('Te', "Now this isnt-", 600, 150, 500); 
+		setTextSize('Te', 29);
+		setObjectCamera('Te', 'other');
+		addLuaText('Te', true);
+
+		setProperty('Text16.visible', false);
+	  end
+	  if curStep == 791 then
+		makeLuaText('Text17', "WHO ASKED YOU!?", 600, 670, 600); 
+		setTextSize('Text17', 29);
+		setTextBorder("Text17", 1, '85E100');
+		setObjectCamera('Text17', 'other');
+		addLuaText('Text17', true);
+
+		setProperty('Text16.visible', false);
+	  end
+	  if curStep == 800 then
+		setProperty('Te.visible', false);
+		setProperty('Text17.visible', false);
+	  end
+	  if curStep == 1168 then
+		makeLuaText('Text19', "OH!", 600, 670, 600); 
+		setTextSize('Text19', 29);
+		setObjectCamera('Text19', 'other');
+		addLuaText('Text19', true);
+		
+	  end
+	  if curStep == 1177 then
+		makeLuaText('Text18', "BF!!!", 600, 670, 600); 
+		setTextSize('Text18', 32);
+		setTextBorder("Text18", 1, '85E100');
+		setObjectCamera('Text18', 'other');
+		addLuaText('Text18', true);
+
+		setProperty('Text19.visible', false);
+	  end
+	  if curStep == 1184 then
+		setProperty('Text18.visible', false);
+	end
+	if curStep == 1442 then
+		makeLuaText('Text20', "They popped a cap in my class", 600, 670, 600); 
+		setTextSize('Text20', 29);
+		setTextBorder("Text20", 1, '85E100');
+		setObjectCamera('Text20', 'other');
+		addLuaText('Text20', true);
+
+		setProperty('Text19.visible', false);
+	  end
+	  if curStep == 1458 then
+		makeLuaText('Text21', "now one will be in your ass", 600, 670, 600); 
+		setTextSize('Text21', 29);
+		setTextBorder("Text21", 1, '85E100');
+		setObjectCamera('Text21', 'other');
+		addLuaText('Text21', true);
+
+		setProperty('Text20.visible', false);
+	  end
+	  if curStep == 1474 then
+		makeLuaText('Text22', "Thought you could solve it with drinking", 600, 670, 600); 
+		setTextSize('Text22', 29);
+		setTextBorder("Text22", 1, '85E100');
+		setObjectCamera('Text22', 'other');
+		addLuaText('Text22', true);
+
+		setProperty('Text21.visible', false);
+	  end
+	  if curStep == 1490 then
+		makeLuaText('Text23', "but now your mental health is sinking", 600, 670, 600); 
+		setTextSize('Text23', 29);
+		setTextBorder("Text23", 1, '85E100');
+		setObjectCamera('Text23', 'other');
+		addLuaText('Text23', true);
+
+		setProperty('Text22.visible', false);
+	  end
+
+	  if curStep == 1506 then
+		makeLuaText('Text24', "imma put you right into comatose", 600, 670, 600); 
+		setTextSize('Text24', 29);
+		setTextBorder("Text24", 1, '85E100');
+		setObjectCamera('Text24', 'other');
+		addLuaText('Text24', true);
+
+		setProperty('Text23.visible', false);
+	  end
+	  if curStep == 1522 then
+		makeLuaText('Text25', "take a shower you smell fuckin gross", 600, 670, 600); 
+		setTextSize('Text25', 29);
+		setTextBorder("Text25", 1, '85E100');
+		setObjectCamera('Text25', 'other');
+		addLuaText('Text25', true);
+
+		setProperty('Text24.visible', false);
+	  end
+	  if curStep == 1538 then
+		makeLuaText('Text26', "Look old man you just got smoked", 600, 670, 600); 
+		setTextSize('Text26', 29);
+		setTextBorder("Text26", 1, '85E100');
+		setObjectCamera('Text26', 'other');
+		addLuaText('Text26', true);
+
+		setProperty('Text25.visible', false);
+	  end
+	  if curStep == 1552 then
+		makeLuaText('Text27', "Now go make another cock joke", 600, 670, 600); 
+		setTextSize('Text27', 29);
+		setTextBorder("Text27", 1, '85E100');
+		setObjectCamera('Text27', 'other');
+		addLuaText('Text27', true);
+
+		setProperty('Text26.visible', false);
+	  end
+	  if curStep == 1568 then
+
+		setProperty('Text27.visible', false);
+	  end
+end
